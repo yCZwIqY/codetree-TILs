99816b055f3205fa3a8c2a915e2d08fdb03c5e0e
jiyoon0605 <ljy10499@gmail.com>
2024-03-25 23:02:39 +0900

    Title: 알파벳과 숫자 출력, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240325/알파벳과 숫자 출력/alphabet-and-numeric-output.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = Number(fs.readFileSync('/dev/stdin'));
+
+let startStr=65;
+let startNum=1;
+
+
+for(let i = 0; i < input; i++) {
+    const arr = [];
+    for(let j = 0; j < input-i; j++) {
+        arr.push(String.fromCharCode(startStr++));
+        if(startStr > 91) {
+            startStr = 65;
+        }    
+    }
+    for(let j = 0; j < i + 1; j++) {
+        arr.push(startNum++);
+        if(startNum > 9) {
+            startNum = 1;
+        }  
+    }
+    console.log(arr.join(' '))
+}
