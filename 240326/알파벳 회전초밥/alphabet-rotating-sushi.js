98253aefd839b36bacd659fc430b5a937525a289
jiyoon0605 <ljy10499@gmail.com>
2024-03-26 23:00:14 +0900

    Title: 알파벳 회전초밥, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 240326/알파벳 회전초밥/alphabet-rotating-sushi.js
+++ 240326/알파벳 회전초밥/alphabet-rotating-sushi.js
@@ -4,16 +4,9 @@ const input = fs.readFileSync('/dev/stdin').toString();
 const a = input.split('\n')[0].split('');
 const b = input.split('\n')[1];
 
-const idx = [];
-
-b.split('').map(it => {
-    idx.push(a.indexOf(it));
-})
-
 let cnt = 1;
-
-idx.reduce((pre, cur) => {
-    if(pre >= cur) {
+b.split('').reduce((pre, cur) => {
+    if(a.indexOf(pre) >= a.indexOf(cur)) {
         cnt++;
     }
     return cur;
