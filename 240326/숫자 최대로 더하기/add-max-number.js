5110e62013ee225af17b2daa8e80795d1340f6a3
jiyoon0605 <ljy10499@gmail.com>
2024-03-26 23:01:38 +0900

    Title: 숫자 최대로 더하기, Time: 325ms, Memory: 25MB, Status: Passed - CodeTree

--- 240326/숫자 최대로 더하기/add-max-number.js
+++ 240326/숫자 최대로 더하기/add-max-number.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString();
 
-const n = Number(input.split('\n')[0]);
 const arr = input.split('\n')[1].split(' ').map(it => Number(it)).sort((a,b) => b-a);
 
 let result = arr.reduce((pre,cur) => {
