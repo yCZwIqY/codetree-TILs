0cc5a7144784c7bca33f91c5cc59e3110d1403b2
jiyoon0605 <ljy10499@gmail.com>
2024-03-26 22:46:43 +0900

    Title: 알파벳 회전초밥, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240326/알파벳 회전초밥/alphabet-rotating-sushi.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+const a = input.split('\n')[0].split('');
+const b = input.split('\n')[1];
+
+const idx = [];
+
+b.split('').map(it => {
+    idx.push(a.indexOf(it));
+})
+
+let cnt = 1;
+
+idx.reduce((pre, cur) => {
+    if(pre > cur) {
+        cnt++;
+    }
+    return cur;
+});
+
+console.log(cnt);
