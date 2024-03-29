747e4c255845f3fad36241cdd24565117081d886
jiyoon0605 <ljy10499@gmail.com>
2024-03-29 21:55:11 +0900

    Title: 코드트리 오마카세, Time: 183ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240329/코드트리 오마카세/codetree-omakase.js
+++ 240329/코드트리 오마카세/codetree-omakase.js
@@ -1,99 +1,83 @@
-const fs= require('fs');
+const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const len = input[0].split(' ')[0];
-const q = input[0].split(' ')[1];
-
-input.shift();
+const [len, q] = input.shift().split(' ').map(Number);
 
 
-const MAKE_SUSHI = '100';
-const VISIT_CUSTOMER = '200';
-const TAKE_PHOTO = '300';
+const MAKE_SUSHI = 100;
+const VISIT_CUSTOMER = 200;
+const TAKE_PHOTO = 300;
 
-let table = Array.from({length: len}, () => []);
-const actions = new Array(q);
+let sushiCount = 0;
+const table = new Array(len);
+let customer = new Array();
+let prevTime = Number.MAX_SAFE_INTEGER;
 
-//시간별 동작
-input.map((it, index) => {
-    const time = it.split(' ')[1];
-    actions[index] = {
-        time: Number(time),
-        [it.split(' ')[0]]: {
-            position: it.split(' ')[2],
-            name: it.split(' ')[3],
-            num: it.split(' ')[4],
-        }
-    };
+const actions = [];
+input.forEach(it => {
+    const [type, time, position, name, num] = it.split(' ').map((x) => (isNaN(x) ? x : Number(x)));
+    prevTime = Math.min(prevTime, time);
+    actions.push({ time, type, position, name, num });
 });
 
 
-const makeSushi = (action) => {
-    //초밥을 테이블에 둠
-    if(action[MAKE_SUSHI]) {
-        sushi++;
-        const position = action[MAKE_SUSHI].position;
-        table[position] 
-            ? table[position].push(action[MAKE_SUSHI])
-            : table[position] = [action[MAKE_SUSHI]];
-    }
+for(let i = 0; i < len; i++) {
+    table[i] = [];
+}
 
-    //손님 입장
-    if(action[VISIT_CUSTOMER]) {
-        consumer.push(action[VISIT_CUSTOMER]);
-    }
 
+for (const action of actions) {
+    const { time, type, position, name, num } = action;
+
+    // // console.log("====================== 현재 시간: " + time + " ======================");
+    // console.log("Table: ");
+    // console.log(table);
+    // // console.log("------------------------------------------------");
+    // console.log("Customer: ");
+    // console.log(customer);
+
+    if (type === MAKE_SUSHI) {
+        sushiCount++;
+        if (!table[position]) table[position] = [];
+        table[position].push({ name });
+    } 
+    
+    if (type === VISIT_CUSTOMER) {
+        customer.push({name, position, num});
+    } 
+    
     //자신의 앞에있는 초밥을 먹음
-    consumer.forEach(it => {
+    customer.forEach(it => {
         const name = it.name;
-        const position = it.position;
+        const customerPosition = it.position;
         const num = it.num;
-        if(table[position].some(dish => dish.name === name)) {
-            const eat = table[position].filter(dish => dish.name === name).length;
-            table[position] = table[position].filter(dish => dish.name !== name);
-        
-            sushi-=eat;
-            it.num = num - eat
+        if((table[customerPosition] ?? []).some(dish => dish.name === name)) {
+            const eat = table[customerPosition].filter(dish => dish.name === name).length;
+            table[customerPosition] = table[customerPosition].filter(dish => dish.name !== name);
+            sushiCount-=eat;
+            it.num = num - eat;
         }
     });
 
     //떠난 손님의 초밥 폐기
     table.forEach(it => {
         it = it.map(i => {
-                return consumer.some(c => c.name === it.name && c.num <= 0) ? null : i;
+                return customer.some(c => c.name === it.name && c.num <= 0) ? null : i;
             }).filter(i => i!== null);
     });
 
-    //사진 촬영
-    if(action[TAKE_PHOTO]) {
-        console.log(consumer.filter(it => it.num > 0).length, sushi);
+    customer = customer.filter(c => c.num > 0);
+    
+    if(type === TAKE_PHOTO) {
+        console.log(customer.length, sushiCount);
     }
-}
 
-
-let sushi = 0;
-let consumer = [];
-
-
-makeSushi(actions[0]);
-
-actions.reduce((pre, cur) => {
-
-    // console.log("====================== 현재 시간: " + cur.time + " ======================");
-    // console.log("Table: ");
-    // console.log(table);
-    // console.log("------------------------------------------------");
-    // console.log("Consumer: ");
-    // console.log(consumer);
-
-    if(pre) {
-        //지나간 시간만큼 로테이션
-        const r = (cur.time - pre.time) % len;
+    
+    // 시간이 변경되었을 때 테이블을 회전시킴
+    if (prevTime !== time) {
+        const r = (prevTime - time) % len;
         const temp = table.splice(table.length - r, table.length);
         table.unshift(...temp);
+        prevTime = time;
     }
-
-    makeSushi(cur);
-
-    return cur;
-});
+}
