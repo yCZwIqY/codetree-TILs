ad9791593f1835c143f1933bd44dd251aedc9b33
jiyoon0605 <ljy10499@gmail.com>
2024-03-30 23:12:39 +0900

    Title: 격자에 점 그리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240330/격자에 점 그리기/draw-points-on-grid.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const [n, m] = input.shift().split(' ').map(it => Number(it));
+
+const coinPos = input;
+
+const grid = Array.from({length: n}, () => Array.from({length: n}, () => 0));
+
+coinPos.map((it, index) => {
+    const [r, c] = it.split(' ').map(it => Number(it) - 1);
+    grid[r][c] = index + 1;
+});
+
+grid.map(it => console.log(it.join(' ')));
