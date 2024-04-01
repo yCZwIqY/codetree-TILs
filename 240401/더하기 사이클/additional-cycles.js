e3830f73bd57f3144e71aedde9c1397c204b9088
jiyoon0605 <ljy10499@gmail.com>
2024-04-01 21:30:26 +0900

    Title: 더하기 사이클, Time: 163ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240401/더하기 사이클/additional-cycles.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+let n = input;
+let count = 1;
+while(count < 10) {
+    count++;
+    const x = n.toString()
+            .split('')
+            .map(it => Number(it))
+            .reduce((pre, cur) => pre+cur, 0)
+            .toString()
+            .split('')
+            .pop();
+    const y = input.split('').pop();
+
+    n = x+y;
+    
+    if(n===input) {
+        console.log(count);
+        return;
+    }
+}
