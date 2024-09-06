257d527bad398296dd2822a7a76801946c451ef4
jiyoon <ljy10499@gmail.com>
2024-09-06 23:58:00 +0900

    Title: 삽입 정렬 구현, Time: 256ms, Memory: 47MB, Status: Passed - CodeTree

--- 240906/삽입 정렬 구현/implement-insertion-sort.js
+++ 240906/삽입 정렬 구현/implement-insertion-sort.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = parseInt(input.shift());
 const arr = input.shift().split(' ').map(it => parseInt(it));
 
