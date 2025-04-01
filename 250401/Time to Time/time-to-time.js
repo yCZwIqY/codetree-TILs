9935aa8ef296894b0ff04da32882e3e2e7f900de
jiyoon <ljy10499@gmail.com>
2025-04-01 21:58:41 +0900

    Title: Time to Time, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250401/Time to Time/time-to-time.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [a, b, c, d] = input[0].split(' ').map(Number);
+// Please Write your code here.
+console.log((c*60+d) - (a*60 + b))
